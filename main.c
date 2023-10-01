6a0029d11cbb3e1a7821bb360db06791ee973316
Matalya <matalya8@gmail.com>
2023-10-01 18:23:39 -0300

    Changed perms for ba.sh

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
+#include <locale.h>
 #include <stdio.h>
+#include <wchar.h>
 
-void printBoard() {
-    
+void printBoard(int width) {
+    printf("%lc", 0x250c);
+    for (int i = 0; i < width; i++) {
+        
+    }
 }
 
 int main() {
-    
+    setlocale(LC_CTYPE, "");
+    printBoard(5);
 }
