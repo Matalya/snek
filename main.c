52a3a6f82a15c65ec28b110895ed3f7377ff5405
Matalya <matalya8@gmail.com>
2023-10-08 15:48:16 -0300

    Updating. Capitalism not yet added.

--- main.c
+++ main.c
@@ -8,17 +8,16 @@
 
 #define fori(x) for (int i = 0; i < x; i++)
 
-struct termios *become_fanon(struct termios *original, struct termios *modified) {
-  // Get the original terminal settings
-  tcgetattr(STDIN_FILENO, original);
-  modified = original;
+void become_fanon(struct termios *original, struct termios *modified) {
+    // Get the original terminal settings
+    tcgetattr(STDIN_FILENO, original);
+    modified = original;
 
-  // Disable canonical mode and echoing
-  modified->c_lflag &= ~(ICANON | ECHO);
+    // Disable canonical mode and echoing
+    modified->c_lflag &= ~(ICANON | ECHO);
 
-  // Set the new terminal settings
-  tcsetattr(STDIN_FILENO, TCSANOW, modified);
-  return original;
+    // Set the new terminal settings
+    tcsetattr(STDIN_FILENO, TCSANOW, modified);
 }
 
 typedef enum {
@@ -76,17 +75,18 @@ wchar_t pickArrow(Dir dir) {
     }
 }
 
-void printBoard(Dir dir) {
+void clearGameArea() {
     //Clear the game area
     for (int col = 2; col <= BOARD_HEIGHT; col++) {
         for (int row = 2; row <= BOARD_WIDTH; row++) {
             printf("%lc[%d;%dH ", ESC, col, row);
         }
     }
-    printf("%lc[2;1H", ESC);
+}
 
+void printBoard(Dir dir) {
     //Print score
-    printf("%lc[33CScore: ??       Direction: %lc\n%lc[3;3", ESC, pickArrow(dir), ESC);
+    printf("%lc[2;1H%lc[33CScore: ??       Direction: %lc\n%lc[3;3", ESC, ESC, pickArrow(dir), ESC);
 
     //Print top
     printf("%lc[3;1H%lc", ESC, TPLF);
@@ -120,7 +120,7 @@ void printSnek(coords coords, wchar_t thechar) {
 
 int gameover(coords pos) {
     int gamestate = 0;
-    if ((pos.x == 0 || pos.x == BOARD_WIDTH + 1) || (pos.y == 2 || pos.y == BOARD_HEIGHT + 1)) {
+    if ((pos.x <= 0 || pos.x >= BOARD_WIDTH) || (pos.y <= 3 || pos.y >= BOARD_HEIGHT)) {
         gamestate = 1;
     }
     return gamestate;
@@ -186,8 +186,7 @@ int kbhit(void) {
 int main() {
     //Setting up some shit, Idk what it's doing.
     struct termios original, modified;
-    struct termios *originalptr;
-    originalptr = become_fanon(&original, &modified);
+    become_fanon(&original, &modified);
     setlocale(LC_CTYPE, "");
 
     Dir snekdir = RIGHT;
@@ -202,16 +201,19 @@ int main() {
     while (!gameover(snekpos)) {
         //Detect keystrokes and updates direction.
         if (kbhit()) {snekdir = pickDirFromInput(getchar(), snekdir);}
+        clearGameArea();
         printBoard(snekdir);
         printSnek(snekpos, FLBK);
         updatePos(snekdir, &snekpos);
         enqueueItem(snekBody, snekpos);
         usleep(snekdir < 2?150000:100000);
     }
-    printSnek(snekpos, ' ');
+    clearGameArea();
+    printBoard(snekdir);
+    printSnek(snekpos, 'X');
     printf("%lc[13;41HGAME OVER%lc[26;1H", ESC, ESC);
     
     // Restore the original terminal settings
-    tcsetattr(STDIN_FILENO, TCSANOW, originalptr);
+    tcsetattr(STDIN_FILENO, TCSANOW, &original);
     return 0;
 }
